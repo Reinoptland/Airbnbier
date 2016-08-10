49719913492bd831743f98a522c5d4c66ed115e7
reinoptland <reinoptland@gmail.com>
2016-08-10 17:32:19 +0200

    added view template, route and controller action for price desc

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/allrooms" => "rooms#list_all_rooms" , as: :all_rooms
   get "/allrooms/priceasc" => "rooms#list_all_rooms_price_asc"
+  get "/allrooms/pricedesc" => "rooms#list_all_rooms_price_desc"
 end
