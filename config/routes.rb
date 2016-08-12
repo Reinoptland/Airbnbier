d8bb61d83108954a044d51a730314731bdd7b0a2
reinoptland <reinoptland@gmail.com>
2016-08-12 15:36:18 +0200

    deleted viewtemplates of removed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :bookings, except: [:index, :show, :edit]
-  resources :rooms
+  resources :rooms, except: [:index]
   root "rooms#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/allrooms" => "rooms#list_all_rooms" , as: :all_rooms
+  get "/rooms" => "rooms#list_all_rooms" 
   get "/allrooms/priceasc" => "rooms#list_all_rooms_price_asc"
   get "/allrooms/pricedesc" => "rooms#list_all_rooms_price_desc"
   get 'user_bookings' => 'bookings#user', as: :user_bookings
