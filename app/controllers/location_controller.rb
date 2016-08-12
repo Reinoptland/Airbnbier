9b15d6b17484d288dae586e0319187e2c705aeb4
reinoptland <reinoptland@gmail.com>
2016-08-12 13:49:10 +0200

    deleted location partial

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,22 +1,27 @@
 class LocationController < ApplicationController
 
   def amsterdam_rooms
+    @user = current_user
     @rooms = Room.where location_id: 1
   end
 
   def utrecht_rooms
+    @user = current_user
     @rooms = Room.where location_id: 2
   end
 
   def denhaag_rooms
+    @user = current_user
     @rooms = Room.where location_id: 3
   end
 
   def rotterdam_rooms
+    @user = current_user
     @rooms = Room.where location_id: 4
   end
 
   def eindhoven_rooms
+    @user = current_user
     @rooms = Room.where location_id: 5
   end
 end
