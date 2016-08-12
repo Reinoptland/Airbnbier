7cb32cd8912f2134398cb3d2e64202b25543635a
reinoptland <reinoptland@gmail.com>
2016-08-12 15:23:52 +0200

    deleted booking edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :bookings, except: [:index, :show]
+  resources :bookings, except: [:index, :show, :edit]
   resources :rooms
   root "rooms#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
